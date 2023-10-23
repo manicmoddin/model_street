3f900fd8c13aac83642c84de3d085c2a0d0270f8
Jimmy Kemp <jimmy@manicmoddin.co.uk>
2023-10-23 20:49:22 +0100

    Added Pedestrian Crossing

--- model_street.ino
+++ model_street.ino
@@ -1,8 +1,10 @@
 int tRed = D1;   
 int tAmber = D2; 
-int tGreen = D3; 
+int tGreen = D3;
+int pButton = D4;
 
-int lightDelay = 1500; 
+int lightDelay = 1500;
+int pWantingToCross = 0;
 
 void redAmber() {
   digitalWrite(tRed, HIGH);
@@ -28,22 +30,34 @@ void red() {
   digitalWrite(tGreen, LOW);
 }
 
+ICACHE_RAM_ATTR void wantingToCross() {
+  Serial.println("Wanting to Cross");
+  //digitalWrite(led, HIGH);
+  pWantingToCross = 1;
+}
+
 void setup() {
+  Serial.begin(115200);
   pinMode(tRed,OUTPUT);   
   pinMode(tAmber,OUTPUT); 
   pinMode(tGreen,OUTPUT); 
+  pinMode(pButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pButton), wantingToCross, RISING);
 
   // Default to red being on
   red();
 }
 
 void loop(){
-  red();
-  delay(lightDelay*4);
-  redAmber();
-  delay(lightDelay);       
-  green();  
-  delay(lightDelay*4);          
-  amber();
-  delay(lightDelay);    
+  if (pWantingToCross == 1) {
+    delay(lightDelay);
+    amber();
+    delay(lightDelay);
+    red();
+    delay(lightDelay*6);
+    redAmber();
+    delay(lightDelay);
+    pWantingToCross = 0;
+  }
+  green();
 }
