4fdb488680a396dfef5fc088c771cb8b0b6ae5ef
Jimmy Kemp <jimmy@manicmoddin.co.uk>
2023-10-23 21:10:08 +0100

    Added Pedestrian Crossing

--- model_street.ino
+++ model_street.ino
@@ -3,6 +3,9 @@ int tAmber = D2;
 int tGreen = D3;
 int pButton = D4;
 int streetLight = D5;
+int pRed = D6;
+int pGreen = D7;
+int pWait = D8;
 int lightSensor = A0;
 
 int lightDelay = 1500;
@@ -33,6 +36,24 @@ void red() {
   digitalWrite(tGreen, LOW);
 }
 
+void pedestrianWait() {
+  digitalWrite(pWait, HIGH);
+  digitalWrite(pRed, HIGH);
+  digitalWrite(pGreen, LOW);
+}
+
+void pedestrianRed() {
+  digitalWrite(pWait, LOW);
+  digitalWrite(pRed, HIGH);
+  digitalWrite(pGreen, LOW);
+}
+
+void pedestrianGreen() {
+  digitalWrite(pWait, LOW);
+  digitalWrite(pRed, LOW);
+  digitalWrite(pGreen, HIGH);
+}
+
 ICACHE_RAM_ATTR void wantingToCross() {
   Serial.println("Wanting to Cross");
   //digitalWrite(led, HIGH);
@@ -44,6 +65,9 @@ void setup() {
   pinMode(tRed,OUTPUT);   
   pinMode(tAmber,OUTPUT); 
   pinMode(tGreen,OUTPUT); 
+  pinMode(pRed, OUTPUT);
+  pinMode(pGreen, OUTPUT);
+  pinMode(pWait, OUTPUT);
   pinMode(streetLight, OUTPUT);
   pinMode(pButton, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(pButton), wantingToCross, RISING);
@@ -53,15 +77,21 @@ void setup() {
 
   // Default to streetlights being off
   digitalWrite(streetLight, LOW);
+
+  // Default to Pedestrian being Red
+  pedestrianRed();
 }
 
 void loop(){
   if (pWantingToCross == 1) {
+    pedestrianWait();
     delay(lightDelay);
     amber();
     delay(lightDelay);
     red();
+    pedestrianGreen();
     delay(lightDelay*6);
+    pedestrianRed();
     redAmber();
     delay(lightDelay);
     pWantingToCross = 0;
@@ -69,11 +99,11 @@ void loop(){
   green();
   lightLevel = analogRead(lightSensor);
   if (lightLevel <= 200) {
-    Serial.println("Lights on");
+    //Serial.println("Lights on");
     digitalWrite(streetLight, HIGH);
   }
   else {
-    Serial.println("Lights Off");
+    //Serial.println("Lights Off");
     digitalWrite(streetLight, LOW);
   }
 }
